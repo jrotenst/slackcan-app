7b80298c46c759378cc0f527729f17392b56e773
yossi <yrotenstreich@gmail.com>
2020-12-28 21:43:51 -0500

    If course can't be retrieved, skip it

--- src/main/java/com/CanvasGetter.java
+++ src/main/java/com/CanvasGetter.java
@@ -101,6 +101,9 @@ public class CanvasGetter {
         
         for(Course course : myCourses) {
             LOG.info("  " + course.getName());
+            if (course == null) {
+                continue;
+            }
 
             assignments = getAssignments(course);
             int i = 1;
